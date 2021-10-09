cba41ec4aacb4969fcda2da61e16f6c447fc0e94
Naila Ghigo <nailaghigo20@gmail.com>
2021-10-09 18:47:09 -0300

    * Arrays: - Solved exercise 3-g - Reorganized all exercises.

--- excersices/03_arrays.js
+++ excersices/03_arrays.js
@@ -3,38 +3,45 @@ console.log('*****Arrays Practice*****');
 Given the following array: ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"],
 show by console, months 5 and 11 (use console.log)*/
 var monthArr = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
+var monthArrCopy = Object.assign([], monthArr);
 
 console.log('3-a) The months at positions 5 and 11 are:');
 console.log(monthArr[5] + " " + monthArr[11]);
+
 /* 3-b
 Order alphabetically the month array and show it by console.(use sort).*/
-var sortedMonthArr = monthArr.sort();
-
 console.log('3-b) This is month array sorted alphabetically:')
-console.log(sortedMonthArr);
+console.log(monthArr.sort());
+
 /* 3-c
 Add one element at the biggining and at the end of the array (use unshift and push).*/
-var elementBefore = sortedMonthArr.unshift('Meses');
-var elementAfter = sortedMonthArr.push(2021);
+monthArr.unshift('Meses');
+monthArr.push(2021);
 
 console.log('This is the result of exercise 3-c:');
-console.log(sortedMonthArr);
+console.log(monthArr);
+
 /* 3-d
 Remove the first element and the last one from the array (use shift and pop).*/
-var quitFirstElem = sortedMonthArr.shift(0);
-var quitLastElem = sortedMonthArr.pop();
+monthArr.shift(0);
+monthArr.pop();
 
 console.log('This is the result of exercise 3-d:');
-console.log(sortedMonthArr);
+console.log(monthArr);
+
 /* 3-e
 invert the array order (use reverse).*/
-var invertOrder = sortedMonthArr.reverse(0);
+monthArr.reverse(0);
 
 console.log('This is the result of exercise 3-e:');
-console.log(invertOrder);
+console.log(monthArr);
+
 /* 3-f
 Join all array elements in one string where a - separate each month (use join)..*/
-var joinArray = sortedMonthArr.join('-');
-
 console.log('This is the result of exercise 3-f:');
-console.log(joinArray);
+console.log(monthArr.join('-'));
+
+/* 3-g
+create a month array copy that contains from may to november (use slice).*/
+console.log('This is the result of exercise 3-g:');
+console.log(monthArrCopy.slice(4, 11))
