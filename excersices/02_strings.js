3486293f719e63fc44a1f4f864348d15c30601ff
Naila Ghigo <nailaghigo20@gmail.com>
2021-10-09 14:02:47 -0300

    * Strings: - Solved exercise 2-e

--- excersices/02_strings.js
+++ excersices/02_strings.js
@@ -15,7 +15,6 @@ console.log('The result for excercise number 2-b is: ' + first5Characters);
 /* 2-c
 - Create a String type variable with at least 10 characters and create a new string with the last 3 characters saving the result in a new variable (use substring) */
 var last3Characters = text.substring(text.length - 3);
-
 console.log('The result for excercise number 2-c is: ' + last3Characters);
 
 /* 2-d
@@ -23,8 +22,14 @@ console.log('The result for excercise number 2-c is: ' + last3Characters);
 var exampleText = 'it is really important to practice JavaScript at least an hour per day';
 
 var firstUpperCase = exampleText.substring(0, 1).toUpperCase() + exampleText.substring(1, exampleText.length).toLowerCase();
-
 console.log('The result for excercise number 2-d is: ' + firstUpperCase);
+
+/* 2-e
+- Create a String type variable with at least 10 characters and some blank space. FInd the first blank space position and save it in a variable. (use indexOf).*/
+
+var blankSpace = exampleText.indexOf(" ");
+console.log('The result for excercise number 2-e is: ' + blankSpace);
+
 
 
 
