5fc7150befcabe0dc3f0609b65020bab73130fc8
Naila Ghigo <nailaghigo20@gmail.com>
2021-10-08 22:23:24 -0300

    * Strings: - Solved excercise 2-b

--- excersices/02_strings.js
+++ excersices/02_strings.js
@@ -1,1 +1,7 @@
 console.log('*****Strings Practice*****');
+/* 2-a
+- Create a String type variable with at least 10 characters and convert all the text to uppercase. */
+var text = "Today is a great day to eat hot pancackes!"
+
+var toUpper = text.toUpperCase()
+console.log('The result for excercise number 2-a is ' + toUpper);
