0651099fea6f69cc05e7bd6f3e933a4edc6f5500
Naila Ghigo <nailaghigo20@gmail.com>
2021-10-09 16:30:34 -0300

    * Arrays: - Solved excercise 3-a

--- excersices/03_arrays.js
+++ excersices/03_arrays.js
@@ -1,1 +1,8 @@
 console.log('*****Arrays Practice*****');
+/* 3-a
+Given the following array: ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"],
+show by console, months 5 and 11 (use console.log)*/
+var monthArr = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+
+console.log('The month at the array position 5 is: ' + monthArr[5] + ', and the 11 position is: ' + monthArr[11]);
+
