e14c68f54b3291e4f1f442922b58abcee4249995
Naila Ghigo <nailaghigo20@gmail.com>
2021-10-09 16:37:09 -0300

    * Arrays: - Solves exercise 3-b

--- excersices/03_arrays.js
+++ excersices/03_arrays.js
@@ -4,5 +4,10 @@ Given the following array: ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio
 show by console, months 5 and 11 (use console.log)*/
 var monthArr = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
 
-console.log('The month at the array position 5 is: ' + monthArr[5] + ', and the 11 position is: ' + monthArr[11]);
+console.log('3-a) The month at the array position 5 is: ' + monthArr[5] + ', and the 11 position is: ' + monthArr[11]);
 
+/* 3-b
+Order alphabetically the month array and show it by console.(use sort).*/
+var sortedMonthArr = monthArr.sort();
+
+console.log('3-b) This is month array sorted alphabetically: ' + sortedMonthArr);
