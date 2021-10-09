4db6e9ff2ac29dc7f3f1419ed11f151ba10655c0
Naila Ghigo <nailaghigo20@gmail.com>
2021-10-08 21:01:38 -0300

    * Variables and Operators: - Solved Excersice 1-b

--- excersices/01_variables_and_operators.js
+++ excersices/01_variables_and_operators.js
@@ -1,8 +1,16 @@
 console.log('*****Variables and Operators Practice*****');
-// 1-a
-// Create two number variables and use the sum operator to save the total value of both in a third variable. 
+/* 1-a
+- Create two number variables and use the sum operator to save the total value of both in a third variable. */
 var numberOne = 20;
 var numberTwo = 30;
 
 var sumResult = numberOne + numberTwo;
 console.log('The result for excercise number 1-a is ' + sumResult);
+
+/* 1-b
+- Create two variables of type String and concatenate them by saving the result in a third variable.. */
+var stringOne = 'Good';
+var stringTwo = 'Morning';
+
+var joinedStrings = stringOne + ' ' + stringTwo;
+console.log('The result for excercise number 1-b is ' + joinedStrings);
