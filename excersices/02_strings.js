d3330bcdd8fc761a3620f2363d49b9b53cce3cf1
Naila Ghigo <nailaghigo20@gmail.com>
2021-10-08 22:43:17 -0300

    * Strings: - Solved exercise 2-b - Changed 2-1 var text.

--- excersices/02_strings.js
+++ excersices/02_strings.js
@@ -1,7 +1,13 @@
 console.log('*****Strings Practice*****');
 /* 2-a
 - Create a String type variable with at least 10 characters and convert all the text to uppercase. */
-var text = "Today is a great day to eat hot pancackes!"
+var text = "Tomorrow will be a great day to eat hot pancackes!"
 
 var toUpper = text.toUpperCase()
 console.log('The result for excercise number 2-a is ' + toUpper);
+
+/* 2-b
+- Create a String type variable with at least 10 characters and create a new string with the first 5 characters saving the result in a new variable (use substring) */
+var first5Characters = text.substring(0, 5);
+
+console.log('The result for excercise number 2-a is ' + first5Characters);
