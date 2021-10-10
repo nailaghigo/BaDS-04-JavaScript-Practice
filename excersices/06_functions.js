9f92827c323ad8c5a8717a41ca6a5630a76fb1e5
Naila Ghigo <nailaghigo20@gmail.com>
2021-10-10 03:00:16 -0300

    * Functions: - Solved

--- excersices/06_functions.js
+++ excersices/06_functions.js
@@ -13,25 +13,36 @@ Execute the function and save the result in a variable, showing the value of tha
 // var totalResult = totalPrice(priceOne, priceTwo);
 // console.log('This is the result of exercise 6-a: ' + totalResult);
 
-/* 6-a
+/* 6-b
 To the previous sum function, add a validation to control if any of the parameters is not a number, 
 show an alert clarifying that one of the parameters has an error and return the NaN value as a result*/
 
-var priceOne = 'hola';
-var priceTwo = 4;
+// var priceOne = 'hola';
+// var priceTwo = 4;
 
-function totalPrice (a, b) {
-    if (!isNaN(a) && !isNaN(b)){
-        return a + b;
-    } else {
-        alert('One of the paramethers has an error')
-        if(isNaN(a)) {
-            return a;
-        } else {
-            return b;
-        }
-    }
+// function totalPrice (a, b) {
+//     if (!isNaN(a) && !isNaN(b)){
+//         return a + b;
+//     } else {
+//         alert('One of the paramethers has an error')
+//         if(isNaN(a)) {
+//             return a;
+//         } else {
+//             return b;
+//         }
+//     }
+// }
+
+// var totalResult = totalPrice(priceOne, priceTwo);
+// console.log('This is the result of exercise 6-b: ' + totalResult)
+
+
+/* 6-c
+Create a validate integer function that takes a number as a parameter and returns true if it is an integer.*/
+
+function validateInteger(value) {
+    return Number.isInteger(value);
 }
 
-var totalResult = totalPrice(priceOne, priceTwo);
-console.log('This is the result of exercise 6-b: ' + totalResult)
+var value = 'Hola';
+console.log('This is the result of exercise 6-c, is "' + value + '" an Integer?: ' + validateInteger(value));
