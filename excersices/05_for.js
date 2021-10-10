c8245d424da682d1b9c4d65d0f9b58322906afaa
Naila Ghigo <nailaghigo20@gmail.com>
2021-10-09 23:19:01 -0300

    * For: - Solved exercise 5-d

--- excersices/05_for.js
+++ excersices/05_for.js
@@ -20,9 +20,20 @@ To the array above convert the first letter of each word to UpperCase and show o
 Create a variable called "sentence" with an empty tring, then iterate the array from 5-a) with a javascript for  to save every word inside the sentence variable
 At the end show an only alert with the complete string. */
 
-var sentence = ' ';
+// var sentence = ' ';
 
-for(var i=0; i < animalsArr.length; i++){
-   sentence += animalsArr[i] + ' ';
+// for(var i=0; i < animalsArr.length; i++){
+//    sentence += animalsArr[i] + ' ';
+// }
+// alert('List of animals: ' + sentence);
+
+/* 5-d
+Create an empty array and loop for 10 reps. Fill the array with the repetition number, that is, at the end of the execution of the for loop,
+there should be 10 elements within the array, from number 0 to number 9. Show the final array through the browser console (use console .Log in).*/
+
+var numbers = [];
+
+for(var i=0; i < 10 ; i++){
+    numbers.push(i);    
 }
-alert('List of animals: ' + sentence);
+console.log(numbers);
