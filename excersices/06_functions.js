48ed66f09c9b828b12c5bf54831c6094ef604d00
Naila Ghigo <nailaghigo20@gmail.com>
2021-10-10 02:42:56 -0300

    * Functions : - solved exercise 6-b

--- excersices/06_functions.js
+++ excersices/06_functions.js
@@ -1,1 +1,37 @@
 console.log('*****Functions Practice*****');
+/* 6-a
+Create a sum function that receives two number values and return the result. 
+Execute the function and save the result in a variable, showing the value of that variable in browser console.*/
+
+// var priceOne = 'hola';
+// var priceTwo = 4;
+
+// function totalPrice (a, b) {
+//     return a + b;
+// }
+
+// var totalResult = totalPrice(priceOne, priceTwo);
+// console.log('This is the result of exercise 6-a: ' + totalResult);
+
+/* 6-a
+To the previous sum function, add a validation to control if any of the parameters is not a number, 
+show an alert clarifying that one of the parameters has an error and return the NaN value as a result*/
+
+var priceOne = 'hola';
+var priceTwo = 4;
+
+function totalPrice (a, b) {
+    if (!isNaN(a) && !isNaN(b)){
+        return a + b;
+    } else {
+        alert('One of the paramethers has an error')
+        if(isNaN(a)) {
+            return a;
+        } else {
+            return b;
+        }
+    }
+}
+
+var totalResult = totalPrice(priceOne, priceTwo);
+console.log('This is the result of exercise 6-b: ' + totalResult)
