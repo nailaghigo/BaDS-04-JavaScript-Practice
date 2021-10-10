708e5567dd0ed3c62352c2ed3474c0cd8dd5debb
Naila Ghigo <nailaghigo20@gmail.com>
2021-10-09 23:12:13 -0300

    * For: - Solved exercise 5-c

--- excersices/05_for.js
+++ excersices/05_for.js
@@ -11,6 +11,18 @@ var animalsArr = ['lobo', 'leon', 'tigre', 'conejo', 'puma'];
 /* 5-b
 To the array above convert the first letter of each word to UpperCase and show one alert for each modified word. */
 
+// for(var i=0; i < animalsArr.length; i++){
+//     alert('My animal is: ' + animalsArr[i].substring(0, 1).toUpperCase());
+// }
+
+
+/* 5-c
+Create a variable called "sentence" with an empty tring, then iterate the array from 5-a) with a javascript for  to save every word inside the sentence variable
+At the end show an only alert with the complete string. */
+
+var sentence = ' ';
+
 for(var i=0; i < animalsArr.length; i++){
-    alert('My animal is: ' + animalsArr[i].substring(0, 1).toUpperCase());
+   sentence += animalsArr[i] + ' ';
 }
+alert('List of animals: ' + sentence);
