b42278d809945370768aa8a8833e01b779a6e080
Naila Ghigo <nailaghigo20@gmail.com>
2021-10-09 22:56:28 -0300

    * For: - Solved exercise 5-b

--- excersices/05_for.js
+++ excersices/05_for.js
@@ -4,8 +4,13 @@ Create an array that contains 5 words and iterate it using javascript for to sho
 
 var animalsArr = ['lobo', 'leon', 'tigre', 'conejo', 'puma'];
 
-for(var i=0; i < animalsArr.length; i++) {
-    alert('My animal is: ' + animalsArr[i]);
+// for(var i=0; i < animalsArr.length; i++) {
+//     alert('My animal is: ' + animalsArr[i]);
+// }
+
+/* 5-b
+To the array above convert the first letter of each word to UpperCase and show one alert for each modified word. */
+
+for(var i=0; i < animalsArr.length; i++){
+    alert('My animal is: ' + animalsArr[i].substring(0, 1).toUpperCase());
 }
-
-
